220a81f5d7e527262addd1764381f002feb652d0
Belyy Igor <belyyigor@gmail.com>
2022-06-09 16:57:23 +0300

    Добавьте файлы проекта.

---
+++ CppProject_2/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	cout << "Start working with github\n";
+
+	cout << "local repositary test\n";
+
+	cin.get();
+	cin.get();
+
+	return 0;
+}
